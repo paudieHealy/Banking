2eeed1fab3e0b604bdb32e7337d38663138bc3e9
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-01-31 17:01:04 +0600

    Test for BankAccount.java

--- src/com/company/BankAccountTest.java
+++ src/com/company/BankAccountTest.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
@@ -7,16 +9,34 @@ public class BankAccountTest {
     @org.junit.Test
     public void deposit() {
        // fail();
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("hasan","jaman",1000,BankAccount.CHECKING);
+        double balance = account.deposit(500,true);
+        assertEquals(1500,balance,0);
     }
 
     @org.junit.Test
     public void withdraw() {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("hasan","jaman",1500,BankAccount.CHECKING);
+        double balance = account.withdraw(800,true);
+        assertEquals(700,balance,0);
     }
 
     @org.junit.Test
     public void getBalance() {
         fail("This test has yet to be implemented");
     }
+    @Test
+    public void getBalance_Withdraw() {
+        BankAccount account = new BankAccount("hasan","jaman",1500,BankAccount.CHECKING);
+        account.withdraw(800,true);
+        assertEquals(700,account.getBalance(),0);
+    }
+
+    @Test
+    public void isChecking_true(){                                                                      // 1
+        BankAccount account = new BankAccount("hasan","jaman",1500,BankAccount.CHECKING);
+        //assertEquals(true,account.isChecking());
+        assertTrue("account is not checking account",account.isChecking());
+    }
+
 }
